f799bf1d0936a4490cc3b0a21034058d148a108c
shivani <shivanirbaheti@gmail.com>
2026-08-19 23:36:25 +0530

    Day-01 Java Practice

---
+++ ArrayQ6.java
@@ -0,0 +1,14 @@
+//Reverse an Array
+
+public class ArrayQ6 {
+    public static void main(String[] args) {
+        int[] arr={2,5,3,8,3,5};
+       
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+      
+}
